7427dc86f7914d1ad934c1943086334ef1870bb7
Jahs <jxtory@sina.com>
2018-08-19 20:50:17 +0800

    201808192050
    游戏基础构建中-镜头特效扩展
    游戏基础构建中-生产特写

--- Assets/Codes/MainControllers.cs
+++ Assets/Codes/MainControllers.cs
@@ -63,7 +63,8 @@ public class MainControllers : MonoBehaviour {
 	[Header("跟随镜头(场景)")]
 	// 场景是否跟随主镜头
 	public bool FollowCamera = false;
-	// 辅助相机目标位置	和 显示效率
+	// 辅助相机目标位置、模式	和 显示效率
+	private int auxCameraMode = -1;
 	private ArrayList auxCameraTarget;
 	private float auxCameraShowTimer = 5f;
 	private float auxCameraShowTimerRun = 0f;
@@ -113,42 +114,75 @@ public class MainControllers : MonoBehaviour {
 	// - 辅助相机控制 -
 	void auxCameraControl()
 	{
-		// 查看辅助相机和任务 如果存在立即投入工作
-		if(auxCamera != null && auxCameraTarget != null && auxCameraTarget.Count > 0){
-			// 设置移动路线
-            Vector3 tg = (Vector3)auxCameraTarget[0];
-			Vector3 tp = auxCamera.transform.position;
+		// 查看辅助模式
+		// 默认模式
+		if(auxCameraMode == 0){
+			// 查看辅助相机和任务 如果存在立即投入工作
+			if(auxCamera != null && auxCameraTarget != null && auxCameraTarget.Count > 0){
+				// 设置移动路线
+	            Vector3 tg = (Vector3)auxCameraTarget[0];
+				Vector3 tp = auxCamera.transform.position;
 
-			// 判断完成度
-            if (Vector3.Distance(tp, tg) > 0.2f){
-            	// 平滑移动和计算轨迹时间
-            	auxCameraShowTimerRun += Time.deltaTime;
-				auxCamera.transform.position = Vector3.SmoothDamp(tp, tg, ref auxCameraVelocity, auxCameraShowTimer / auxCameraShowTime);
+				// 判断完成度
+	            if (Vector3.Distance(tp, tg) > 0.2f){
+	            	// 平滑移动和计算轨迹时间
+	            	auxCameraShowTimerRun += Time.deltaTime;
+					auxCamera.transform.position = Vector3.SmoothDamp(tp, tg, ref auxCameraVelocity, (auxCameraShowTimer / auxCameraShowTime - auxCameraShowTimerRun));
 
-			} else{
-				//auxCamera.transform.position = Vector3.SmoothDamp(tp, tg, ref auxCameraVelocity, auxCameraShowTimer / auxCameraShowTime);
-				// 接近时候辅助完成，并在单轨迹时间到达时清空轨迹
-				auxCamera.transform.position = tg;
-                auxCameraVelocity = Vector3.zero;
-				// 计算轨迹时间
-            	auxCameraShowTimerRun += Time.deltaTime;
-            	if(auxCameraShowTimerRun >= auxCameraShowTimer){
-	                auxCameraTarget.Remove(tg);
-	                auxCameraShowTimerRun = 0;
-            	}
-			}
+				} else{
+					//auxCamera.transform.position = Vector3.SmoothDamp(tp, tg, ref auxCameraVelocity, auxCameraShowTimer / auxCameraShowTime);
+					// 接近时候辅助完成，并在单轨迹时间到达时清空轨迹
+					auxCamera.transform.position = tg;
+	                auxCameraVelocity = Vector3.zero;
+					// 计算轨迹时间
+	            	auxCameraShowTimerRun += Time.deltaTime;
+	            	if(auxCameraShowTimerRun >= (auxCameraShowTimer / auxCameraShowTime)){
+		                auxCameraTarget.Remove(tg);
+		                auxCameraShowTimerRun = 0;
+	            	}
+				}
 
-			// 始终保持镜头最前
-			//auxCamera.transform.localPosition = new Vector3(auxCamera.transform.localPosition.x, auxCamera.transform.localPosition.y, (-10 - auxCamera.transform.localPosition.z));
+				// 始终保持镜头最前
+				//auxCamera.transform.localPosition = new Vector3(auxCamera.transform.localPosition.x, auxCamera.transform.localPosition.y, (-10 - auxCamera.transform.localPosition.z));
 
-		} else {
-			// 如果任务结束 卸载辅助相机
-			if(auxCameraTarget.Count == 0 && auxCamera != null){
-				Destroy(auxCamera);
-				auxCamera = null;
+			} else {
+				// 如果任务结束 卸载辅助相机
+				if(auxCameraTarget.Count == 0 && auxCamera != null){
+					/*
+						Destroy(auxCamera);
+						auxCameraMode = -1;
+						auxCamera = null;
+					*/
+					// 清除摄像机, 感谢, 后台领盒饭去！
+					ClearAuxCamera();
+				}
 			}
 		}
 
+		// 跟随模式
+		if(auxCameraMode == 1){
+			// 查看是否为跟随模式
+			if(auxCamera != null && auxCameraTarget != null && auxCameraTarget.Count == 1){
+				// 计算轨迹时间 执行跟随
+				GameObject him = auxCameraTarget[0] as GameObject;
+				auxCameraShowTimerRun += Time.deltaTime;
+				auxCamera.transform.position = him.transform.position;
+				auxCamera.transform.position += new Vector3(0, 0, (-10 - him.transform.position.z));
+				// 如果任务结束 卸载辅助相机
+				if(auxCameraShowTimerRun >= auxCameraShowTimer){
+					// 清除摄像机
+					ClearAuxCamera();
+				}
+
+			} else {
+				auxCameraShowTimerRun += Time.deltaTime;
+				// 如果任务结束 卸载辅助相机
+				if(auxCameraShowTimerRun >= auxCameraShowTimer){
+					// 清除摄像机
+					ClearAuxCamera();
+				}
+			}
+		}
 
 	}
 
@@ -176,6 +210,21 @@ public class MainControllers : MonoBehaviour {
 				borderRight.transform.position = new Vector3((float)((Screen.width * 1.0f / Screen.height) * Camera.main.orthographicSize) + cmx, cmy, 0);
 			}
 		}
+	}
+
+	// - 清除辅助相机参数 -
+	void ClearAuxCamera()
+	{
+		//	if(auxCamera.gameObject != null){Destroy(auxCamera.gameObject);}
+		if(auxCamera != null){Destroy(auxCamera.gameObject);}
+		auxCamera = null;
+		auxCameraMode = -1;
+		auxCameraTarget = null;
+		auxCameraShowTimer = 5f;
+		auxCameraShowTimerRun = 0f;
+		auxCameraShowTime = 5;
+		auxCameraVelocity = Vector3.zero;
+
 	}
 
 	// - 清除子物体 -
@@ -219,21 +268,58 @@ public class MainControllers : MonoBehaviour {
 
 	}
 
-	// - 创建辅助相机 -
-	void createAuxCamera(Vector3 pos, Vector3[] target, float f_time = 5f, bool coverMode = true)
+	// - 创建新的辅助相机 - 
+	void createAuxCamera(Vector3 pos, bool f_orthographic = true)
 	{
+		// 清除相机
+		ClearAuxCamera();
 		// 新建相机
-		if(auxCamera.gameObject != null){Destroy(auxCamera.gameObject);}
 		auxCamera = new GameObject();
+		auxCamera.name = "auxCamera";
 		auxCamera.AddComponent<Camera>();
 		auxCamera.transform.position = pos;
 		auxCamera.transform.localPosition += new Vector3(0, 0, -10);
+		auxCamera.GetComponent<Camera>().orthographic = f_orthographic;
+	}
+
+	// - 创建辅助相机 跟随模式-
+	void createAuxCamera(GameObject him, float f_time = 3f, bool follow = true)
+	{
+		// 创建相机
+		createAuxCamera(him.transform.position);
 		auxCamera.GetComponent<Camera>().backgroundColor = new Color(25 / 255, 25 / 255, 25 / 255, 1f);
-		auxCamera.GetComponent<Camera>().orthographic = true;
+
+		// 辅助跟随模式
+		auxCameraMode = 1;
+
+		// 设置任务轨迹
+		if(follow){
+			auxCameraTarget = new ArrayList();
+			auxCameraTarget.Add(him);
+		}
+
+		// 设置任务时间
+		auxCameraShowTimer = f_time;
+
+		// 设置任务次数
+		auxCameraShowTime = 1;
+
+	}
+
+	// - 创建辅助相机 默认模式-
+	void createAuxCamera(Vector3 pos, Vector3[] target, float f_time = 5f, bool coverMode = true)
+	{
+		// 创建相机
+		createAuxCamera(pos);
+		auxCamera.GetComponent<Camera>().backgroundColor = new Color(25 / 255, 25 / 255, 25 / 255, 1f);
+
+		// 辅助默认模式
+		auxCameraMode = 0;
+
+		// 调整覆盖模式
 		if(!coverMode){
 			auxCamera.GetComponent<Camera>().rect = new Rect(0.7f, 0.7f, 1f, 1f);
 		}
-		auxCamera.name = "auxCamera";
 
 		// 设置任务轨迹
 		auxCameraTarget = new ArrayList();
@@ -289,6 +375,11 @@ public class MainControllers : MonoBehaviour {
 	    // 将盒子加入到大集合
 	    Boxs.Add(newBox);
 
+	    // 随机播放生产特写
+	    if(Random.Range(0,100) < 20){
+	    	createAuxCamera(newBox, 4f);
+	    }
+
 	}
 
 	// - 入口控制 -
@@ -329,6 +420,12 @@ public class MainControllers : MonoBehaviour {
 	GameObject FindIt(string him)
 	{
 		return GameObject.Find(him);
+	}
+
+	// - 获取相机坐标 -
+	Vector3 GetMainCameraPos()
+	{
+		return Camera.main.transform.position;
 	}
 
 	// - 获取盒子框 -
@@ -450,9 +547,9 @@ public class MainControllers : MonoBehaviour {
 	{
 		if(spawnPoint != new Vector3()){
 			if(joyBallSpawnPoint != new Vector3()){
-				createAuxCamera(Camera.main.transform.position, new Vector3[]{Camera.main.transform.position, spawnPoint, joyBallSpawnPoint, Camera.main.transform.position}, 3f, false);
+				createAuxCamera(GetMainCameraPos(), new Vector3[]{GetMainCameraPos() ,spawnPoint, joyBallSpawnPoint, GetMainCameraPos(), GetMainCameraPos()}, 12.5f, false);
 			} else {
-				createAuxCamera(Camera.main.transform.position, new Vector3[]{Camera.main.transform.position, spawnPoint, Camera.main.transform.position}, 3f);
+				createAuxCamera(GetMainCameraPos(), new Vector3[]{GetMainCameraPos(), spawnPoint, GetMainCameraPos()}, 6f);
 
 			}
 
